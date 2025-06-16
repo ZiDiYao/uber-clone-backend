a7606ea7cb9005e57908491eaa499c8c9b124b01
ZiDiYao <zidiyaocanada@outlook.com>
2025-06-16 13:05:24 +0800

    update

--- common/common-log/src/main/java/org/zidi/entity/LogRecord.java
+++ common/common-log/src/main/java/org/zidi/entity/LogRecord.java
@@ -1,6 +1,16 @@
 package org.zidi.entity;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class LogRecord {
-    // 用于记入到数据库
-    // Log has been recorded
+    private Long id;
+    private Long customerId;
+    private String ipaddr;
+    private Integer status;      // 可用于标记成功/失败，0/1
+    private String msg;          // 操作描述
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private Boolean isDeleted;
 }
---
+++ common/service-util/src/main/java/org/zidi/common/util/JwtUtils.java
@@ -0,0 +1,10 @@
+package org.zidi.common.util;
+
+/**
+ * A Class that aims to create Token when user sign in or sign up
+ * Redis
+ * Key Token Value : CustomId
+ */
+
+public class JwtUtils {
+}
